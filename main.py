1b7b270adb7644d228e7d769c39dec06b05a7a8d
Max <massimo.odisio@gmail.com>
2025-04-23 14:39:09 +0200

    add print styled report on main

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ def main():
     num_words = word_counter(text)
     char_dic = char_counter(text)
     ordered_list = order_dic(char_dic)
-    #print(f"{num_words} words found in the document")
-    #print(f"{char_dic}")
-    print(f"{ordered_list}")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for element in ordered_list:
+        if element["char"].isalpha():
+            print(f"{element["char"]}: {element["num"]}")
+    print("============= END ===============")
 
 main()

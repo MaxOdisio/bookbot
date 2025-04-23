c2cf9082028a9ae5c95e3c825812e334366b286d
Max <massimo.odisio@gmail.com>
2025-04-23 15:07:08 +0200

    add new books and set input via sys

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import sys
+
 from stats import char_counter, get_book_text, order_dic, word_counter
 
 
 # Main return
-def main():
-    text = get_book_text("books/frankenstein.txt")
+def main(file):
+    text = get_book_text(file)
     num_words = word_counter(text)
     char_dic = char_counter(text)
     ordered_list = order_dic(char_dic)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {file}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
@@ -17,4 +19,8 @@ def main():
             print(f"{element["char"]}: {element["num"]}")
     print("============= END ===============")
 
-main()
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main(sys.argv[1])
